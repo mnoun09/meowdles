c6f334d58e7af9f3ce2c96dc1ae04e399779a174
mnoun09 <monikanoun2009@gmail.com>
2026-01-17 18:53:03 -0800

    Imported temp assets, Worked on basic functionality
    - Added buttons leading to different scenes
    - Clicking on the pot makes the soup visible

---
+++ stuff/soup_pot.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+signal potClicked
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and event.is_pressed():
+		print("click detected")
+		emit_signal("potClicked")
+	pass # Replace with function body.
