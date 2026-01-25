5ea01ab3189d25b5cc7be5d208db619d7a0cc517
mnoun09 <monikanoun2009@gmail.com>
2026-01-24 17:53:26 -0800

    Added home and shop button
    - shop menu wip

--- menu.gd
+++ menu.gd
@@ -1,5 +1,9 @@
 extends MarginContainer
 
+@export var menuScreen: VBoxContainer
+@export var shopScreen: VBoxContainer
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +13,27 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func toggleVisibility(object):
+	if object.visible:
+		object.visible = false
+	else:
+		object.visible = true
+	pass
+
+
+func _on_top_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
+	pass # Replace with function body.
+
+
+func _on_middle_pressed() -> void:
+	toggleVisibility($"../StoreMenu")
+	toggleVisibility($".")
+	pass # Replace with function body.
+
+
+func _on_button_pressed() -> void:
+	toggleVisibility($"../StoreMenu")
+	toggleVisibility($".")
+	pass # Replace with function body.
